4e6724aaf3cce1f8b47ed3db53abdd399e712966
unknown <galinbar2411@gmail.com>
2024-11-20 18:13:55 +0200

    init project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const postRoutes = require("./routes/postRoutes");
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_CONNECTION);
+const db = mongoose.connection;
+db.on("error", (e) => console.log(e));
+db.once("open", () => console.log("connected to db"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/posts", postRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+});
