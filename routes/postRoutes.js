d71ae7d40a79818cac9a0e32d09c1b72405d403f
unknown <galinbar2411@gmail.com>
2024-11-23 14:17:25 +0200

    fix cr commentg

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post("/", post.addNewPost);
 router.get("/", post.getAllPosts);
 router.get("/:id", post.getPostById);
-router.get("/sender/:senderId", post.getPostsBySenderId);
+router.get("/sender=/:senderId", post.getPostsBySenderId);
 router.put("/:id", post.updatePost);
 
 module.exports = router;
